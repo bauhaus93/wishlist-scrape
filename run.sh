59b4f6953638ffc58bf822b8b60e921159956a5d
Jakob Fischer <jakobfischer93@gmail.com>
2020-10-25 17:01:41 +0100

    Fixed scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-if [ ! -d 'venv' ]; then
-	$PWD/setup.sh
-fi
-venv/bin/python3 main.py
+FILE=$(realpath $0)
+DIR=${FILE%/*}
+$DIR/venv/bin/python3 main.py
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d 'venv' ]; then
-	python3 -mvenv venv
-fi
-
-venv/bin/pip install --upgrade pip &&
-	venv/bin/pip install -r requirements.txt
